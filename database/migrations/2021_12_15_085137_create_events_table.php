416c0ceb4b3dc08e4811e2eb5543056d4dc9020a
ClaudiaCalero <claudiaclrduro@gmail.com>
2021-12-16 11:32:19 +0100

    change data

--- database/migrations/2021_12_15_085137_create_events_table.php
+++ database/migrations/2021_12_15_085137_create_events_table.php
@@ -18,7 +18,7 @@ class CreateEventsTable extends Migration
             $table->string('title');
             $table->string('img');
             $table->string('text');
-            $table->timestamps('date');
+            $table->date('date');
             $table->timestamps();
         });
     }
