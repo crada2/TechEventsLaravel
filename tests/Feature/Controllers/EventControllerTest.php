3463bfc82c28f3e0bf135fa3b4509f4df6655fe1
Sol <solcis.bio@gmail.com>
2022-01-19 11:25:22 +0100

    tests admin

--- tests/Feature/Controllers/EventControllerTest.php
+++ tests/Feature/Controllers/EventControllerTest.php
@@ -25,6 +25,35 @@ class EventControllerTest extends TestCase
 
         $response->assertStatus(302)
                 ->assertRedirect("/login");
-    }         
+    }
+
+    public function test_admin_can_create_an_event()
+   {
+    $admin = User::factory()->create(["isAdmin"=>true]);
+    $event = Event::factory()->create();
+
+    $response = $this->actingAs($admin)->get(route("events.create", $event->id));
+    $response->assertStatus(200)->assertViewIs('admin.eventForm');
+   }
+    
+   public function test_admin_can_edit_event()
+   {
+       $admin = User::factory()->create(["isAdmin"=>true]);
+       $event = Event::factory()->create();
+
+       $response = $this->actingAs($admin)->get(route("edit", $event->id));
+       $response->assertStatus(200)->assertViewIs('admin.editEventform');
+   }
+
+   public function test_admin_can_update_event()
+   {
+    $admin = User::factory()->create(["isAdmin"=>true]);
+    $event = Event::factory()->create();
+
+    $response = $this->actingAs($admin)->get(route("update", $event->id));
+    $response->assertStatus(200)->assertViewIs('admin.editEventform');
+   }
+
+
 
 }
--- tests/Feature/Controllers/LandingControllerTest.php
+++ tests/Feature/Controllers/LandingControllerTest.php
@@ -17,32 +17,12 @@ class LandingControllerTest extends TestCase
      *
      * @return void
      */
-    public function test_can_see_a_landing_page()
+    public function test_any_user_can_see_a_landing_page()
     {
         $response = $this->get(route('landing'));
 
         $response->assertStatus(200)
         ->assertViewIs('landing');
     }
-
-    public function test_any_user_can_see_list_events_future()
-    {
-        User::factory()->create();
-        //crear 2 Eventos en la db con factoria
-        $events=Event::factory(2)->create();
-        
-        //when SUT llamar una ruta (web.php)
-        $response = $this->get(route('landing'));
-
-        //then
-        $response->assertSee($events[0]->title)
-                    ->assertSee($events[1]->title);
-                    
-
-    }
-
-    
-
-
 }
 
