ec0a1550031165fa253e71aa24949d2983528956
Ceci <carbel.cecilia@gmail.com>
2022-01-21 08:44:12 +0100

    change frontend login and register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,39 @@
+<style>
+    .card-header:first-child {
+
+    background-color: lightpink;
+    text-align: center;
+    font-size: 1.5rem;
+}
+.card {
+   /* position: relative;
+    display: flex;
+    flex-direction: column;
+    min-width: 0px;
+    word-wrap: break-word;*/
+    background-color: #65b1c9;
+
+}
+.mb-3 {
+    margin-bottom: 1rem !important;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.card-body {
+    flex: 1 1 auto;
+    padding: 1rem 1rem;
+    background-color: #b5d7f9;
+}
+.col-form-label {
+
+    text-align: center;
+
+}
+</style>
+
+
 @extends('layouts.app')
 
 <x-header />
@@ -6,7 +42,12 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">{{ __('Login') }}
+                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                      </svg>
+                </div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,31 @@
+<style>
+    .card-header:first-child {
+    background-color: lightpink;
+    text-align: center;
+    font-size: 1.5rem;
+}
+.card {
+    background-color: #65b1c9;
+}
+.mb-3 {
+    margin-bottom: 1rem !important;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.card-body {
+    flex: 1 1 auto;
+    padding: 1rem 1rem;
+    background-color: #b5d7f9;
+}
+.col-form-label {
+
+    text-align: center;
+
+}
+</style>
+
 @extends('layouts.app')
 <x-header />
 @section('content')
@@ -6,7 +34,12 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header">{{ __('Register') }}
+                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-person-check-fill" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+                        <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                      </svg>
+                </div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
@@ -62,8 +95,8 @@
                             </div>
                         </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                        <div class="row mb-0 ">
+                            <div class="d-flex justify-content-center">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Register') }}
                                 </button>
